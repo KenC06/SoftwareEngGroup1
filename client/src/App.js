962adf33d95da3d53c2690a34e403dd7c76cab28
elliotcraft79 <16109561+elliotcraft79@users.noreply.github.com>
2025-11-02 20:29:40 +1030

    Add tailwind and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,11 @@
 import Inventory from "./pages/Inventory";
-export default function App() { return <Inventory />; }
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+export default function App() { return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/inventory" element={<Inventory />} />
+        </Routes>
+    </Router>
+); }
